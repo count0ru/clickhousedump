0af46f76805472b8c7d802dd4d7b571f44e196dc
Dmitry Nagovitsin <count0ru@gmail.com>
2018-09-11 19:44:15 +0300

    make project structure fit for gb

---
+++ src/fileutils/fileutils.go
@@ -0,0 +1,145 @@
+package fileutils
+
+import (
+	"os"
+	"io/ioutil"
+	logs "logging"
+	"path"
+	"io"
+	"strings"
+)
+
+// Recursive copy directory and files
+func CopyDirectory(sourceDirectory string, destinationDirectory string) error {
+
+	var (
+		err             error
+		fileDescriptors []os.FileInfo
+		sourceInfo      os.FileInfo
+	)
+
+	if sourceInfo, err = os.Stat(sourceDirectory); err != nil {
+		return err
+	}
+
+	if err = os.MkdirAll(destinationDirectory, sourceInfo.Mode()); err != nil {
+		return err
+	}
+
+	if fileDescriptors, err = ioutil.ReadDir(sourceDirectory); err != nil {
+		return err
+	}
+	for _, fileDescriptor := range fileDescriptors {
+		sourcePath := path.Join(sourceDirectory, fileDescriptor.Name())
+		destinationPath := path.Join(destinationDirectory, fileDescriptor.Name())
+		if fileDescriptor.IsDir() {
+			if err = CopyDirectory(sourcePath, destinationPath); err != nil {
+				logs.Error.Fatalln(err)
+			}
+		} else {
+			if err = CopyFile(sourcePath, destinationPath); err != nil {
+				logs.Error.Fatalln(err)
+			}
+		}
+	}
+	return nil
+}
+
+// Copy files
+func CopyFile(sourceFile string, destinationFile string) error {
+
+	var err error
+
+	fromFile, err := os.Open(sourceFile)
+	if err != nil {
+		return err
+	}
+
+	defer fromFile.Close()
+
+	toFile, err := os.OpenFile(destinationFile, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		return err
+	}
+	defer toFile.Close()
+
+	_, err = io.Copy(toFile, fromFile)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Replace string in all files in directory
+func ReplaceStringInDirectoryFiles(filesPath string, oldString string, newString string) error {
+	var (
+		err             error
+		fileDescriptors []os.FileInfo
+	)
+
+	if fileDescriptors, err = ioutil.ReadDir(filesPath); err != nil {
+		return err
+	}
+
+	for _, fileDescriptor := range fileDescriptors {
+		if !fileDescriptor.IsDir() && strings.HasSuffix(fileDescriptor.Name(), ".sql") {
+
+			fileContent, err := ioutil.ReadFile(filesPath + "/" + fileDescriptor.Name())
+			if err != nil {
+				return err
+			}
+
+			newContent := strings.Replace(
+				string(fileContent),
+				oldString,
+				newString,
+				-1,
+			)
+
+			err = ioutil.WriteFile(filesPath+"/"+fileDescriptor.Name(), []byte(newContent), 0)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+// Check directory list is exist
+func IsDirectoryInListExist(directoriesList ...string) (error, string) {
+	logs.Info.Println(directoriesList)
+	for _, currentDirectory := range directoriesList {
+		_, err := IsExists(currentDirectory)
+		if err != nil {
+			return err, currentDirectory
+		}
+	}
+	return nil, ""
+}
+
+// Create list of directories
+func CreateDirectories(directoriesList []string) (error, string) {
+	for _, currentDirectory := range directoriesList {
+		directoryExists, err := IsExists(currentDirectory)
+		if err != nil {
+			if !directoryExists {
+				err := os.Mkdir(currentDirectory, os.ModePerm)
+				if err != nil {
+					return err, currentDirectory
+				}
+			}
+		}
+	}
+	return nil, ""
+}
+
+// Check single directory or file exist
+func IsExists(filePath string) (bool, error) {
+	var err error
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		return false, err
+	}
+	return true, err
+}
---
+++ src/restore/restore.go
@@ -0,0 +1,157 @@
+package restore
+
+import (
+	logs "logging"
+	parts "partutils"
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+type RestoreDatabase struct {
+	DatabaseName         string
+	SourceDirectory      string
+	DestinationDirectory string
+}
+
+// Restore database
+func (rb *RestoreDatabase) Run(databaseConnection *sqlx.DB) error {
+
+	type metadataFiles struct {
+		fileName,
+		objectName,
+		objectType,
+		metaData string
+	}
+	var (
+		err             error
+		fileDescriptors []os.FileInfo
+		metaFiles       []metadataFiles
+	)
+
+	logs.Info.Printf("try to create database %v", rb.DatabaseName)
+	_, err = databaseConnection.Exec(fmt.Sprintf("CREATE DATABASE %v", rb.DatabaseName))
+	if err != nil {
+		logs.Error.Printf("failed to create database %v", rb.DatabaseName)
+		return err
+	} else {
+		logs.Info.Println("success")
+	}
+
+	if fileDescriptors, err = ioutil.ReadDir(rb.SourceDirectory + "/metadata/" + rb.DatabaseName); err != nil {
+		return err
+	}
+	if err != nil {
+		logs.Error.Printf("can't replace string in metadata files, %v", err)
+	} else {
+		logs.Info.Println("success")
+	}
+
+	for _, fileDescriptor := range fileDescriptors {
+		if !fileDescriptor.IsDir() && strings.HasSuffix(fileDescriptor.Name(), ".sql") {
+
+			logs.Info.Printf("try to read from metadata file %v", fileDescriptor.Name())
+			fileContent, err := ioutil.ReadFile(rb.SourceDirectory + "/metadata/" + rb.DatabaseName + "/" + fileDescriptor.Name())
+			if err != nil {
+				logs.Info.Printf("cant't read from metadata file %v", fileDescriptor.Name())
+				return err
+			} else {
+				logs.Info.Println("success")
+				if strings.HasPrefix(string(fileContent[:]), "CREATE TABLE") { // if object is TABLE
+					metaFiles = append(metaFiles, metadataFiles{
+						fileDescriptor.Name(),
+						strings.Replace(fileDescriptor.Name(), ".sql", "", -1),
+						"table",
+						string(fileContent[:]),
+					})
+				} else if strings.HasPrefix(string(fileContent[:]), "CREATE MATERIALIZED VIEW") { // if object is view
+					metaFiles = append(metaFiles, metadataFiles{
+						fileDescriptor.Name(),
+						strings.Replace(fileDescriptor.Name(), ".sql", "", -1),
+						"view",
+						string(fileContent[:]),
+					})
+				} else { // is other type object
+					metaFiles = append(metaFiles, metadataFiles{
+						fileDescriptor.Name(),
+						strings.Replace(fileDescriptor.Name(), ".sql", "", -1),
+						"other",
+						string(fileContent[:]),
+					})
+				}
+
+			}
+		}
+	}
+
+	// create only tables first
+	for _, metadataFile := range metaFiles {
+		if metadataFile.objectType == "table" {
+			logs.Info.Printf("try to apply metadata from file %v", metadataFile.fileName)
+			_, err = databaseConnection.Exec(
+				strings.Replace(
+					metadataFile.metaData,
+					"CREATE TABLE ",
+					"CREATE TABLE "+rb.DatabaseName+".", -1))
+			if err != nil {
+				logs.Info.Printf("cant't apply metadata file %v", metadataFile.fileName)
+				return err
+			} else {
+				logs.Info.Println("success")
+			}
+
+			logs.Info.Printf("try to attach partitions for %v", rb.DatabaseName+"."+metadataFile.fileName)
+			cmdGetPartitionsListFromDir := parts.GetPartitionsListFromDir{
+				SourceDirectory:      rb.SourceDirectory,
+				DestinationDirectory: rb.DestinationDirectory,
+				DatabaseName:         rb.DatabaseName,
+				TableName:            metadataFile.objectName,
+			}
+			err = cmdGetPartitionsListFromDir.Run()
+			if err != nil {
+				logs.Error.Printf("can't get partition list for attach, %v", err)
+			}
+			partitionsList := cmdGetPartitionsListFromDir.Result
+			for _, attachedPart := range partitionsList {
+				// attach partition
+				logs.Info.Printf("ALTER TABLE %v.%v ATTACH PARTITION '%v'",
+					attachedPart.DatabaseName,
+					attachedPart.TableName,
+					attachedPart.PartID)
+				_, err = databaseConnection.Exec(
+					"ALTER TABLE " + attachedPart.DatabaseName + "." + attachedPart.TableName + " ATTACH PARTITION '" + attachedPart.PartID + "';")
+				if err != nil {
+					logs.Info.Printf("cant't attach partition %v to %v table in %v database, %v",
+						attachedPart.PartID,
+						attachedPart.TableName,
+						attachedPart.DatabaseName, err)
+					return err
+				} else {
+					logs.Info.Println("success")
+				}
+			}
+		}
+	}
+	// create another objects
+	for _, metadataFile := range metaFiles {
+		if metadataFile.objectType != "table" {
+			logs.Info.Printf("try to apply metadata from file %v", metadataFile.fileName)
+			_, err = databaseConnection.Exec(
+				strings.Replace(
+					metadataFile.metaData,
+					metadataFile.objectName,
+					rb.DatabaseName+"."+metadataFile.objectName, -1))
+			if err != nil {
+				logs.Info.Printf("cant't apply metadata file %v", metadataFile.fileName)
+				return err
+			} else {
+				logs.Info.Println("success")
+			}
+		}
+	}
+
+	return nil
+
+}
